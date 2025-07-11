21042ae785bd0b7c02b5235d3604f1ff66a406cf
gamboel99 <eko.w.a@students.kahuripan.ac.id>
2025-07-11 19:14:34 +0700

    Add files via upload

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,4 @@
+
 import os
 import pandas as pd
 from PIL import Image, UnidentifiedImageError
@@ -13,13 +14,13 @@ def load_iklan():
         return pd.DataFrame(columns=["judul", "deskripsi", "harga", "kategori", "kontak", "gambar", "waktu"])
 
 def save_iklan(data):
-    os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)  # Buat folder data/
+    os.makedirs(os.path.dirname(DATA_PATH), exist_ok=True)
     df = load_iklan()
     df = pd.concat([df, pd.DataFrame([data])], ignore_index=True)
     df.to_csv(DATA_PATH, index=False)
 
 def save_image(uploaded_file):
-    os.makedirs(IMAGE_DIR, exist_ok=True)  # Buat folder images/
+    os.makedirs(IMAGE_DIR, exist_ok=True)
     try:
         filename = datetime.now().strftime("%Y%m%d%H%M%S_") + uploaded_file.name.replace(" ", "_")
         filepath = os.path.join(IMAGE_DIR, filename)
